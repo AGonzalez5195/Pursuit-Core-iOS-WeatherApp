51fa4f2273121c8bc59827fae48a6694f352c38a
AGonzalez5195 <AnthonyGonzalez@pursuit.org>
2019-10-19 17:37:10 -0400

    restricts textfield to only take in 5 numbers

--- WeatherApp/WeatherApp/Controllers/ViewController.swift
+++ WeatherApp/WeatherApp/Controllers/ViewController.swift
@@ -20,143 +20,143 @@ class ViewController: UIViewController {
         collectionView.dataSource = self
         collectionView.delegate = self
         collectionView.register(WeatherCollectionViewCell.self, forCellWithReuseIdentifier: "weatherCell")
+        
+        return collectionView
+    }()
     
-    return collectionView
-}()
-
-lazy var titleLabel: UILabel = {
-    let label = UILabel()
-    label.text = "Weather Forecast"
-    label.translatesAutoresizingMaskIntoConstraints = false
-    label.textAlignment = .center
-    return label
-}()
-
-lazy var zipCodeTextField: UITextField = {
-    let textView = UITextField()
-    textView.translatesAutoresizingMaskIntoConstraints = false
-    textView.placeholder = "Enter Zipcode..."
-    textView.borderStyle = .line
-    textView.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-    textView.delegate = self
-    return textView
-}()
-
-private var weatherData = [WeatherForecast]() {
-    didSet {
-        weatherCollectionView.reloadData()
-    }
-}
-
-private var zipCode: String? {
-    didSet {
-        loadLatLongNameFromZip()
-        weatherCollectionView.isHidden = false
-    }
-}
-
-private var locationName : String? {
-    didSet{
-        titleLabel.text = "Weekly Forecast For \(locationName ?? "BlahBlah")"
-    }
-}
-
-private var latitude: Double?
-private var longitude: Double?
-
-private func setConstraints() {
-    setCollectionViewConstraints()
-    setTitleLabelConstraints()
-    setTextFieldConstraints()
-}
-
-
-private func setCollectionViewConstraints() {
-    NSLayoutConstraint.activate([
-        weatherCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 170),
-        weatherCollectionView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-        weatherCollectionView.widthAnchor.constraint(equalToConstant: weatherCollectionView.frame.width),
-        weatherCollectionView.heightAnchor.constraint(equalToConstant: 155)
-    ])
-}
-
-private func setTitleLabelConstraints() {
-    NSLayoutConstraint.activate([
-        titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 95),
-        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-        titleLabel.widthAnchor.constraint(equalToConstant: 300),
-        titleLabel.heightAnchor.constraint(equalToConstant: 80)
-    ])
-}
-
-private func setTextFieldConstraints() {
-    NSLayoutConstraint.activate([
-        zipCodeTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-        zipCodeTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -60),
-        zipCodeTextField.widthAnchor.constraint(equalToConstant: 140),
-        zipCodeTextField.heightAnchor.constraint(equalToConstant: 40)
-    ])
-}
-
-private func addSubviews(){
-    let UIElements = [weatherCollectionView, titleLabel, zipCodeTextField]
-    for UIElement in UIElements {
-        view.addSubview(UIElement)
-    }
-}
-
-private func loadData() {
-    DarkSkyAPIManager.shared.getForecast(lat: latitude ?? 98765, long: longitude ?? 98765, completionHandler: { (result) in
-        DispatchQueue.main.async {
-            switch result {
-            case .success(let weatherDataFromOnline):
-                self.weatherData = weatherDataFromOnline
-                
-            case .failure(_): ()
-            }
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Weather Forecast"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        return label
+    }()
+    
+    lazy var zipCodeTextField: UITextField = {
+        let textView = UITextField()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.placeholder = "Enter Zipcode..."
+        textView.borderStyle = .line
+        textView.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        textView.delegate = self
+        return textView
+    }()
+    
+    private var weatherData = [WeatherForecast]() {
+        didSet {
+            weatherCollectionView.reloadData()
         }
-    })
-}
-
-private func loadLatLongNameFromZip(){
-    ZipCodeHelper.getLatLongAndName(fromZipCode: zipCode!, completionHandler: { (result) in
-        DispatchQueue.main.async {
-            switch result {
-            case .success(let zipData):
-                self.locationName = zipData.name
-                self.longitude = zipData.long
-                self.latitude = zipData.lat
-                self.loadData()
-                
-            case .failure(_): ()
-            }
+    }
+    
+    private var zipCode: String? {
+        didSet {
+            loadLatLongNameFromZip()
+            weatherCollectionView.isHidden = false
         }
-    })
-}
-
-override func viewWillAppear(_ animated: Bool) {
-    navigationController?.navigationBar.barTintColor = #colorLiteral(red: 1, green: 0.9833787084, blue: 0.8849565387, alpha: 1)
-    navigationController?.navigationBar.backgroundColor = #colorLiteral(red: 1, green: 0.9833787084, blue: 0.8849565387, alpha: 1)
-    navigationController?.navigationBar.barStyle = .default
+    }
     
-    tabBarController?.tabBar.backgroundColor = #colorLiteral(red: 1, green: 0.9833787084, blue: 0.8849565387, alpha: 1)
-    tabBarController?.tabBar.barStyle = .default
-    tabBarController?.tabBar.barTintColor = #colorLiteral(red: 1, green: 0.9833787084, blue: 0.8849565387, alpha: 1)
-}
-
-override func viewDidLoad() {
-    super.viewDidLoad()
-    view.backgroundColor = #colorLiteral(red: 1, green: 0.9833787084, blue: 0.8849565387, alpha: 1)
-    addSubviews()
-    setConstraints()
-    loadData()
+    private var locationName : String? {
+        didSet{
+            titleLabel.text = "Weekly Forecast For \(locationName ?? "BlahBlah")"
+        }
+    }
     
-    guard let savedZipCode = UserDefaultsWrapper.shared.getZipCode() else {
-        weatherCollectionView.isHidden = true
-        return }
-    zipCode = savedZipCode
-    zipCodeTextField.text = savedZipCode
-}
+    private var latitude: Double?
+    private var longitude: Double?
+    
+    private func setConstraints() {
+        setCollectionViewConstraints()
+        setTitleLabelConstraints()
+        setTextFieldConstraints()
+    }
+    
+    
+    private func setCollectionViewConstraints() {
+        NSLayoutConstraint.activate([
+            weatherCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 170),
+            weatherCollectionView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            weatherCollectionView.widthAnchor.constraint(equalToConstant: weatherCollectionView.frame.width),
+            weatherCollectionView.heightAnchor.constraint(equalToConstant: 155)
+        ])
+    }
+    
+    private func setTitleLabelConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 95),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.widthAnchor.constraint(equalToConstant: 300),
+            titleLabel.heightAnchor.constraint(equalToConstant: 80)
+        ])
+    }
+    
+    private func setTextFieldConstraints() {
+        NSLayoutConstraint.activate([
+            zipCodeTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            zipCodeTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -60),
+            zipCodeTextField.widthAnchor.constraint(equalToConstant: 140),
+            zipCodeTextField.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+    
+    private func addSubviews(){
+        let UIElements = [weatherCollectionView, titleLabel, zipCodeTextField]
+        for UIElement in UIElements {
+            view.addSubview(UIElement)
+        }
+    }
+    
+    private func loadData() {
+        DarkSkyAPIManager.shared.getForecast(lat: latitude ?? 98765, long: longitude ?? 98765, completionHandler: { (result) in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let weatherDataFromOnline):
+                    self.weatherData = weatherDataFromOnline
+                    
+                case .failure(_): ()
+                }
+            }
+        })
+    }
+    
+    private func loadLatLongNameFromZip(){
+        ZipCodeHelper.getLatLongAndName(fromZipCode: zipCode!, completionHandler: { (result) in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let zipData):
+                    self.locationName = zipData.name
+                    self.longitude = zipData.long
+                    self.latitude = zipData.lat
+                    self.loadData()
+                    
+                case .failure(_): ()
+                }
+            }
+        })
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.navigationBar.barTintColor = #colorLiteral(red: 1, green: 0.9833787084, blue: 0.8849565387, alpha: 1)
+        navigationController?.navigationBar.backgroundColor = #colorLiteral(red: 1, green: 0.9833787084, blue: 0.8849565387, alpha: 1)
+        navigationController?.navigationBar.barStyle = .default
+        
+        tabBarController?.tabBar.backgroundColor = #colorLiteral(red: 1, green: 0.9833787084, blue: 0.8849565387, alpha: 1)
+        tabBarController?.tabBar.barStyle = .default
+        tabBarController?.tabBar.barTintColor = #colorLiteral(red: 1, green: 0.9833787084, blue: 0.8849565387, alpha: 1)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 1, green: 0.9833787084, blue: 0.8849565387, alpha: 1)
+        addSubviews()
+        setConstraints()
+        loadData()
+        
+        guard let savedZipCode = UserDefaultsWrapper.shared.getZipCode() else {
+            weatherCollectionView.isHidden = true
+            return }
+        zipCode = savedZipCode
+        zipCodeTextField.text = savedZipCode
+    }
 }
 
 extension ViewController: UICollectionViewDataSource {
@@ -202,4 +202,17 @@ extension ViewController: UITextFieldDelegate {
         textField.resignFirstResponder()
         return true
     }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard CharacterSet(charactersIn: "0123456789").isSuperset(of: CharacterSet(charactersIn: string)) else {
+            return false
+        }
+        if range.location == 0 && (string == " ") { return false }
+        
+        let currentText = textField.text ?? ""
+        guard let stringRange = Range(range, in: currentText) else { return false }
+        let updatedText = currentText.replacingCharacters(in: stringRange, with: string)
+        return updatedText.count <= 5
+    }
 }
+
